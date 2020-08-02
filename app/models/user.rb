d2082455dccd97ae3763bc8750f77afdba00144b
tranducminh <minhscdhpvn@gmail.com>
2020-08-02 16:36:14 +0700

    Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.validations.user.email.regex
   USERS_PARAMS = %i(name email password password_confirmation).freeze
-  
-  validates :name, presence: true, 
-    length: {maximum: Settings.validations.user.name.max_length}
 
+  validates :name, presence: true,
+    length: {maximum: Settings.validations.user.name.max_length}
   validates :email, presence: true,
     length: {maximum: Settings.validations.user.email.max_length},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness:  {case_sensitive: false}
-
-  validates :password, presence: true, 
+  validates :password, presence: true,
     length: {minimum: Settings.validations.user.password.min_length}
 
   has_secure_password
@@ -22,5 +20,4 @@ class User < ApplicationRecord
   def downcase_email
     email.downcase!
   end
-  
 end
