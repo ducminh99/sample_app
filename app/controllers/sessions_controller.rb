a075aa9c50e51bcfd58e1224c5f9d3659501538c
tranducminh <minhscdhpvn@gmail.com>
2020-08-04 16:13:50 +0700

    Chapter 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
+    if authenticated_password? user
       log_in user
       if params[:session][:remember_me] == Settings.checkbox.checked_value
         remember user
       else
         forget user
       end
-      redirect_to user
+      redirect_back_or user
 
     else
       flash[:danger] = t "sessions.new.error_message"
@@ -21,5 +21,11 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def authenticated_password? user
+    user&.authenticate params[:session][:password]
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :index, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.page params[:page]
+  end
+
   def show
-    @user = User.find params[:id]
+    @user = find_user_by_id
   end
 
   def new
@@ -19,9 +27,55 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = find_user_by_id
+  end
+
+  def update
+    @user = find_user_by_id
+    if @user.update user_params
+      flash[:success] = t "users.edit.update_success"
+      redirect_to @user
+    else
+      flash[:danger] = t "users.edit.update_failed"
+      redirect_to edit_user_path
+    end
+  end
+
+  def destroy
+    find_user_by_id.destroy
+    flash[:success] = t "users.destroy.deleted_user"
+    redirect_to users_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit User::USERS_PARAMS
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "users.required_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = find_user_by_id
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user_by_id user_id = params[:id]
+    user = User.find_by id: user_id
+    return user if user.present?
+
+    flash[:danger] = t "users.user_not_found"
+    redirect_to root_url
   end
 end
